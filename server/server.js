94b4b216d59627ed86336c5a0a04420d99a314dc
stachmatzidou <stachmatzidou@gmail.com>
2023-02-07 12:01:24 +0200

    Added middleware

--- server/server.js
+++ server/server.js
@@ -2,6 +2,9 @@ import express from "express";
 // import dotenv from "dotenv";
 // dotenv.config();
 import "dotenv/config";
+import cors from "cors";
+import morgan from "morgan";
+import cookieParser from "cookie-parser";
 import mongoose from "mongoose";
 import "colors";
 
@@ -9,6 +12,12 @@ import "colors";
 const PORT = process.env.PORT || 8000;
 //initialize express
 const app = express();
+
+//adding middleware
+app.use(cors());
+app.use(morgan("tiny"));
+app.use(express.json());
+app.use(cookieParser());
 
 //connect to database
 const connectDB = async () => {
