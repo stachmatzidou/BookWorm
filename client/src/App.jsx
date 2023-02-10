9465220dacefd821c43c891c87c5a55b576d1b3b
stachmatzidou <stachmatzidou@gmail.com>
2023-02-10 15:09:07 +0200

    Installed react-router-dom and did some more cleaning up

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,12 @@
-import { useState } from "react";
-import reactLogo from "./assets/logo.png";
 import "./App.css";
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
     return (
+            <Routes>
+                <Route path="/" element={<div>BookWorm</div>}></Route>
+            </Routes>
     );
 }
 
