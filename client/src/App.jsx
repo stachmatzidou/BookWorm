b9301bb1df09683a5f5edec436b85447486c9fc9
stachmatzidou <stachmatzidou@gmail.com>
2023-02-10 15:03:05 +0200

    Installed Vite and did some cleaning up

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import reactLogo from "./assets/logo.png";
+import "./App.css";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+function App() {
+    return (
+    );
+}
+
+export default App;
