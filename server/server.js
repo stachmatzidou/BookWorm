35049c987a4d8c59482dc184f9a43c365b260bda
stachmatzidou <stachmatzidou@gmail.com>
2023-02-25 19:53:58 +0200

    Ready for deployment

--- server/server.js
+++ server/server.js
@@ -32,6 +32,14 @@ app.use((error, req, res, next) => {
     return res.status(status).json({ message, stack: error.stack });
 });
 
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("../client/dist"));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve("../client", "dist", "index.html"));
+    });
+}
+
 //connect to database
 const connectDB = async () => {
     mongoose.set("strictQuery", false);
